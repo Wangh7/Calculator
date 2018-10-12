cf848775a5e3caf9d6b902a31f47510c4a740c19
Wangh7 <978102365@qq.com>
2018-10-13 01:53:49 +0800

    增加了 “加法”功能 修改了 部分Button的UI 简单的两数加法(可循环相加) 将乘除法按钮图标修改为数学符号

--- app/src/main/java/com/example/wbb/calculator/MainActivity.java
+++ app/src/main/java/com/example/wbb/calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.wbb.calculator;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -7,43 +8,127 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public String exp="0";
+    public String exp = "0";
+    public int a = 0;
+    public int state = 0;//0 is null,1 is add,2 is sub,3 is mul,4 is div
     public Button[] buttons = new Button[11];
-    public int[] flags = new int[]{R.id.bt_num0,R.id.bt_num1,R.id.bt_num2,R.id.bt_num3,R.id.bt_num4,
-    R.id.bt_num5,R.id.bt_num6,R.id.bt_num7,R.id.bt_num8,R.id.bt_num9,R.id.bt_clear};
+    public Button[] operations = new Button[5];
+    public int[] flags = new int[]{R.id.bt_num0, R.id.bt_num1, R.id.bt_num2, R.id.bt_num3, R.id.bt_num4,
+            R.id.bt_num5, R.id.bt_num6, R.id.bt_num7, R.id.bt_num8, R.id.bt_num9, R.id.bt_clear};
+    public int[] flags_ope = new int[]{R.id.bt_equ, R.id.bt_add, R.id.bt_sub, R.id.bt_mul, R.id.bt_div};
+
     //public TextView tx = (TextView) findViewById(R.id.tx_bus);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        for(int i=0;i<flags.length;i++){
-            buttons[i] = (Button)findViewById(flags[i]);
+        for (int i = 0; i < flags.length; i++) {
+            buttons[i] = (Button) findViewById(flags[i]);
         }
-        buttons[0].setOnClickListener(v->click_show("0"));
-        buttons[1].setOnClickListener(v->click_show("1"));
-        buttons[2].setOnClickListener(v->click_show("2"));
-        buttons[3].setOnClickListener(v->click_show("3"));
-        buttons[4].setOnClickListener(v->click_show("4"));
-        buttons[5].setOnClickListener(v->click_show("5"));
-        buttons[6].setOnClickListener(v->click_show("6"));
-        buttons[7].setOnClickListener(v->click_show("7"));
-        buttons[8].setOnClickListener(v->click_show("8"));
-        buttons[9].setOnClickListener(v->click_show("9"));
-        buttons[10].setOnClickListener(v->click_clear());
+        for (int i = 0; i < flags_ope.length; i++)
+            operations[i] = (Button) findViewById(flags_ope[i]);
+        buttons[0].setOnClickListener(v -> click_show("0"));
+        buttons[1].setOnClickListener(v -> click_show("1"));
+        buttons[2].setOnClickListener(v -> click_show("2"));
+        buttons[3].setOnClickListener(v -> click_show("3"));
+        buttons[4].setOnClickListener(v -> click_show("4"));
+        buttons[5].setOnClickListener(v -> click_show("5"));
+        buttons[6].setOnClickListener(v -> click_show("6"));
+        buttons[7].setOnClickListener(v -> click_show("7"));
+        buttons[8].setOnClickListener(v -> click_show("8"));
+        buttons[9].setOnClickListener(v -> click_show("9"));
+        buttons[10].setOnClickListener(v -> click_clear());
+        operations[1].setOnClickListener(v -> click_add());
+        operations[2].setOnClickListener(v -> click_sub());
+        operations[3].setOnClickListener(v -> click_mul());
+        operations[4].setOnClickListener(v -> click_div());
+        operations[0].setOnClickListener(v -> click_equ());
     }
-    public void click_show(String i){
-        if(exp.equals("0"))
-            exp=i;
+
+    public void click_show(String i) {
+        resume_color();
+        if (exp.equals("0"))
+            exp = i;
         else
-            exp=exp+i;
-        show();
+            exp = exp + i;
+        show(exp);
     }
-    public void click_clear(){
-        exp="0";
-        show();
+
+    public void click_clear() {
+        exp = "0";
+        a = 0;
+        show(exp);
     }
-    public void show(){
+
+    public void trans() {
+        a = Integer.valueOf(exp);
+    }
+
+    public void resume_color(){
+        operations[1].setTextColor(Color.BLACK);
+        operations[2].setTextColor(Color.BLACK);
+        operations[3].setTextColor(Color.BLACK);
+        operations[4].setTextColor(Color.BLACK);
+    }
+    public void change_color(int i){
+        resume_color();
+        operations[i].setTextColor(Color.rgb(255,220,0));
+    }
+    public void click_add() {
+        change_color(1);
+        if (state == 0 && a != 0)
+            state = 1;
+        equ();
+        state = 1;
+    }
+
+    public void click_sub() {
+
+        state = 2;
+    }
+
+    public void click_mul() {
+
+        state = 3;
+    }
+
+    public void click_div() {
+
+        state = 4;
+    }
+
+    public void equ() {
+        switch (state) {
+            case 0:
+                trans();
+                break;
+            case 1:
+                a = a + Integer.valueOf(exp);
+                break;
+            case 2:
+                a = a - Integer.valueOf(exp);
+                break;
+            case 3:
+                a = a * Integer.valueOf(exp);
+                break;
+            case 4:
+                a = a / Integer.valueOf(exp);
+                break;
+        }
+        exp = "0";
+        show(String.valueOf(a));
+    }
+
+    public void click_equ() {
+        if (state != 0) {
+            equ();
+            state = 0;
+            resume_color();
+        }
+    }
+
+    public void show(String a) {
         TextView tx = (TextView) findViewById(R.id.tx_bus);
-        tx.setText(exp);
+        tx.setText(a);
     }
 }
