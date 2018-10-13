190a5c8a18f99b34d936996a5a1dbf82c547476b
Wangh7 <978102365@qq.com>
2018-10-13 18:08:06 +0800

    增加了 小数、负数运算功能 通过Button更改小数、负数类型

--- app/src/main/java/com/example/wbb/calculator/MainActivity.java
+++ app/src/main/java/com/example/wbb/calculator/MainActivity.java
@@ -10,25 +10,29 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     public String exp = "0";
-    public int a = 0;
+    public double a = 0;
     public int click = 0;
+    public int dot = 0;
     public int state = 0;//0 is null,1 is add,2 is sub,3 is mul,4 is div
     public Button[] buttons = new Button[11];
     public Button[] operations = new Button[5];
+    public Button[] functions = new Button[3];
     public int[] flags = new int[]{R.id.bt_num0, R.id.bt_num1, R.id.bt_num2, R.id.bt_num3, R.id.bt_num4,
             R.id.bt_num5, R.id.bt_num6, R.id.bt_num7, R.id.bt_num8, R.id.bt_num9, R.id.bt_clear};
     public int[] flags_ope = new int[]{R.id.bt_equ, R.id.bt_add, R.id.bt_sub, R.id.bt_mul, R.id.bt_div};
+    public int[] flags_fun = new int[]{R.id.bt_fuhao, R.id.bt_clear, R.id.bt_dot};
 
     //public TextView tx = (TextView) findViewById(R.id.tx_bus);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        for (int i = 0; i < flags.length; i++) {
+        for (int i = 0; i < flags.length; i++)
             buttons[i] = (Button) findViewById(flags[i]);
-        }
         for (int i = 0; i < flags_ope.length; i++)
             operations[i] = (Button) findViewById(flags_ope[i]);
+        for (int i = 0; i < flags_fun.length; i++)
+            functions[i] = (Button) findViewById(flags_fun[i]);
         buttons[0].setOnClickListener(v -> click_show("0"));
         buttons[1].setOnClickListener(v -> click_show("1"));
         buttons[2].setOnClickListener(v -> click_show("2"));
@@ -39,12 +43,14 @@ public class MainActivity extends AppCompatActivity {
         buttons[7].setOnClickListener(v -> click_show("7"));
         buttons[8].setOnClickListener(v -> click_show("8"));
         buttons[9].setOnClickListener(v -> click_show("9"));
-        buttons[10].setOnClickListener(v -> click_clear());
-        operations[1].setOnClickListener(v -> click_add());
-        operations[2].setOnClickListener(v -> click_sub());
-        operations[3].setOnClickListener(v -> click_mul());
-        operations[4].setOnClickListener(v -> click_div());
-        operations[0].setOnClickListener(v -> click_equ());
+        functions[0].setOnClickListener(v -> click_fuhao());//+-
+        functions[1].setOnClickListener(v -> click_clear());//AC
+        functions[2].setOnClickListener(v -> click_dot());//.
+        operations[0].setOnClickListener(v -> click_equ());//eql
+        operations[1].setOnClickListener(v -> click_add());//add
+        operations[2].setOnClickListener(v -> click_sub());//sub
+        operations[3].setOnClickListener(v -> click_mul());//mul
+        operations[4].setOnClickListener(v -> click_div());//div
     }
 
     public void click_show(String i) {
@@ -57,16 +63,47 @@ public class MainActivity extends AppCompatActivity {
         click = 1;
     }
 
+    public void click_dot() {
+        if (dot == 0) {
+            if (exp.equals("0"))
+                exp = "0.";
+            else
+                exp = exp + ".";
+            show(exp);
+        }
+        dot = 1;
+    }
+
+    public void click_fuhao() {
+        double test;
+        if (state == 0 && a != 0) {
+            test = -1 * a;
+            a = test;
+            if (Math.round(test) - test == 0)
+                show(String.valueOf((int) test));
+            else
+                show(String.valueOf(test));
+        } else {
+            test = -1 * Double.valueOf(exp);
+            if (Math.round(test) - test == 0)
+                exp = String.valueOf((int) test);
+            else
+                exp = String.valueOf(test);
+            show(exp);
+        }
+    }
+
     public void click_clear() {
         exp = "0";
         a = 0;
         state = 0;
+        dot = 0;
         resume_color();
         show(exp);
     }
 
     public void trans() {
-        a = Integer.valueOf(exp);
+        a = Double.valueOf(exp);
     }
 
     public void resume_color() {
@@ -88,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
         equ();
         state = 1;
         click = 0;
+        dot = 0;
     }
 
     public void click_sub() {
@@ -99,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
         equ();
         state = 2;
         click = 0;
+        dot = 0;
     }
 
     public void click_mul() {
@@ -110,6 +149,7 @@ public class MainActivity extends AppCompatActivity {
             state = 3;
         }
         click = 0;
+        dot = 0;
     }
 
     public void click_div() {
@@ -121,6 +161,7 @@ public class MainActivity extends AppCompatActivity {
             state = 4;
         }
         click = 0;
+        dot = 0;
     }
 
     public void equ() {
@@ -129,25 +170,29 @@ public class MainActivity extends AppCompatActivity {
                 trans();
                 break;
             case 1:
-                a = a + Integer.valueOf(exp);
+                a = a + Double.valueOf(exp);
                 break;
             case 2:
-                a = a - Integer.valueOf(exp);
+                a = a - Double.valueOf(exp);
                 break;
             case 3:
-                a = a * Integer.valueOf(exp);
+                a = a * Double.valueOf(exp);
                 break;
             case 4:
                 try {
-                    a = a / Integer.valueOf(exp);
-                }catch (Exception e){
-                    Toast.makeText(MainActivity.this,"Error!",Toast.LENGTH_SHORT).show();
+                    a = a / Double.valueOf(exp);
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_SHORT).show();
                     click_clear();
                 }
                 break;
         }
+
         exp = "0";
-        show(String.valueOf(a));
+        if (Math.round(a) - a == 0)
+            show(String.valueOf((int) a));
+        else
+            show(String.valueOf(a));
     }
 
     public void click_equ() {
@@ -157,10 +202,11 @@ public class MainActivity extends AppCompatActivity {
             state = 0;
             resume_color();
         }
+        dot = 0;
     }
 
-    public void show(String a) {
+    public void show(String text) {
         TextView tx = (TextView) findViewById(R.id.tx_bus);
-        tx.setText(a);
+        tx.setText(text);
     }
 }
